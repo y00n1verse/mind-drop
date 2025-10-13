8c1d52c0631f574733d0943f37dbf980e3ff5acf
홍지윤 <jiphrodite@gmail.com>
2025-10-14 00:44:34 +0900

    chore: next/font를 사용하여 폰트 로딩 최적화 및 글로벌 스타일 글꼴 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import './globals.css';
+import { Noto_Sans } from 'next/font/google';
+
+const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+});
 
 export default function RootLayout({
   children,
@@ -7,19 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
+      <body className={`${notoSans.className}`}>
         <header className="h-20">헤더</header>
         <main>{children}</main>
       </body>
