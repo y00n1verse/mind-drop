a2e2db454bd6776bae405a765dd9dfe72229ad2b
홍지윤 <jiphrodite@gmail.com>
2025-10-21 21:28:17 +0900

    feat: 세션 전역 관리를 위해 sessionWrapper 컴포넌트 추가 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Metadata } from 'next';
 import { Noto_Sans } from 'next/font/google';
+import SessionWrapper from './session-wrapper';
 
 const notoSans = Noto_Sans({
   subsets: ['latin'],
@@ -23,8 +24,10 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${notoSans.className}`}>
-        <header className="h-20">헤더</header>
-        <main>{children}</main>
+        <SessionWrapper>
+          <header className="h-20">헤더</header>
+          <main>{children}</main>
+        </SessionWrapper>
       </body>
     </html>
   );
