9c7df1584c4bb72a7dcef68807607935c89ab55e
홍지윤 <jiphrodite@gmail.com>
2025-10-27 15:59:04 +0900

    refactor: 감정 데이터와 아이콘을 constants/emotions.ts으로 분리

---
+++ src/constants/emotions.ts
@@ -0,0 +1,40 @@
+import BestIcon from '@/assets/icons/BestIcon.svg';
+import GoodIcon from '@/assets/icons/GoodIcon.svg';
+import NormalIcon from '@/assets/icons/NormalIcon.svg';
+import BadIcon from '@/assets/icons/BadIcon.svg';
+import WorstIcon from '@/assets/icons/WorstIcon.svg';
+
+export const emotions = [
+  {
+    label: '최고',
+    variant: 'best',
+    Icon: BestIcon,
+    color: 'text-[var(--color-best)]',
+  },
+  {
+    label: '좋음',
+    variant: 'good',
+    Icon: GoodIcon,
+    color: 'text-[var(--color-good)]',
+  },
+  {
+    label: '보통',
+    variant: 'normal',
+    Icon: NormalIcon,
+    color: 'text-[var(--color-normal)]',
+  },
+  {
+    label: '별로',
+    variant: 'bad',
+    Icon: BadIcon,
+    color: 'text-[var(--color-bad)]',
+  },
+  {
+    label: '최악',
+    variant: 'worst',
+    Icon: WorstIcon,
+    color: 'text-[var(--color-worst)]',
+  },
+] as const;
+
+export type EmotionType = (typeof emotions)[number]['variant'];
