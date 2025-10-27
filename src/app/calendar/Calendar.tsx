2104ad8ba0699aabc8de22ad657ff04974fdb25e
홍지윤 <jiphrodite@gmail.com>
2025-10-27 22:22:50 +0900

    feat: useDiaryStore 연동 및 달력에 감정 아이콘 표시 기능 추가(mock-data)
    - 컴포넌트 마운트 시 오늘 날짜가 default로 선택되게 설정

--- src/app/calendar/Calendar.tsx
+++ src/app/calendar/Calendar.tsx
@@ -1,30 +1,62 @@
 'use client';
 
+import { emotions } from '@/constants/emotions';
+import { useDiaryStore } from '@/stores/useDiaryStore';
 import { ko } from 'date-fns/locale';
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/style.css';
 
 export default function Calendar() {
-  const [selected, setSelected] = useState<Date>();
   const today = new Date();
+  const { diaries, selectedDate, setSelectedDate } = useDiaryStore();
+
+  useEffect(() => {
+    const formatted = today.toLocaleDateString('sv-SE');
+    setSelectedDate(formatted);
+  }, [setSelectedDate]);
 
   return (
     <div className="p-4 md:p-5 lg:p-8">
       <DayPicker
-        mode="single"
         locale={ko}
-        selected={selected}
+        mode="single"
         navLayout="around"
-        onSelect={setSelected}
+        selected={selectedDate ? new Date(selectedDate) : undefined}
+        onSelect={(date) => {
+          if (date) {
+            const formatted = date.toLocaleDateString('sv-SE');
+            setSelectedDate(formatted);
+          }
+        }}
         disabled={{ after: today }}
         classNames={{
-          today: 'text-green-600',
-          chevron: 'mb-6 md:mb-4',
-          caption_label:
-            'flex justify-center items-center mb-6 text-base font-semibold md:mb-4 md:text-xl',
           day_button:
-            'flex justify-center items-start text-xs p-2 w-17 h-15 rounded-md hover:bg-gray-100 transition-colors md:text-xl md:w-19 md:h-17 lg:w-24 lg:h-22 lg:text-xl',
+            'flex flex-col cursor-pointer justify-start items-center p-1 gap-1 text-xs  w-17 h-15 rounded-md hover:bg-gray-100 transition-colors lg:p-1.5 lg:gap-1 md:w-21 md:h-19 lg:w-24 lg:h-22',
+          selected: 'rounded-lg bg-gray-100',
+        }}
+        components={{
+          DayButton: ({ day, modifiers, ...buttonProps }) => {
+            const formatted = day.date.toLocaleDateString('sv-SE');
+            const diary = diaries.find((d) => d.date === formatted);
+            const emotionData = emotions.find(
+              (e) => e.variant === diary?.emotion,
+            );
+
+            return (
+              <button
+                {...buttonProps}
+                className={`${buttonProps.className ?? ''}`}
+              >
+                <span>{day.date.getDate()}</span>
+                {emotionData && (
+                  <emotionData.Icon
+                    className={`lg: h-10 w-10 md:h-12 md:w-12 lg:h-14 lg:w-14 ${emotionData.color}`}
+                  />
+                )}
+              </button>
+            );
+          },
         }}
       />
     </div>
