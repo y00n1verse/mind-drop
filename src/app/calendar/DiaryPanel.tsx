4ffb11e0de4a5c187c9c9ea823e212c597b8ca87
홍지윤 <jiphrodite@gmail.com>
2025-10-28 16:47:33 +0900

    feat: DiaryPanel 구현
    - 일기 데이터가 존재할 때 날짜/제목/내용/감정 아이콘 렌더링

--- src/app/calendar/DiaryPanel.tsx
+++ src/app/calendar/DiaryPanel.tsx
@@ -1,16 +1,65 @@
+'use client';
+
+import { useDiaryStore } from '@/stores/useDiaryStore';
+import { emotions } from '@/constants/emotions';
 import Button from '../components/common/Button';
 
 export default function DiaryPanel() {
-  return (
-    <div className="m-5 flex h-40 flex-col items-center justify-center gap-8 rounded-md bg-[oklch(0.937_0_0)] p-4 md:m-0 md:h-150 md:w-full md:p-5 lg:p-8">
-      <div className="text-center">
-        <p>아직 작성된 일기가 없어요</p>
-        <p>일기를 작성하러 갈까요?</p>
-      </div>
+  const { selectedDate, getDiaryByDate } = useDiaryStore();
 
-      <Button variant="complete" size="large">
-        일기쓰러 가기
-      </Button>
+  const diary = selectedDate ? getDiaryByDate(selectedDate) : null;
+
+  if (!diary) {
+    return (
+      <div className="m-5 flex h-40 flex-col items-center justify-center gap-6 rounded-md bg-[oklch(0.937_0_0)] p-4 md:m-0 md:h-150 md:w-full md:gap-8 md:p-5 lg:p-8">
+        <div className="text-center">
+          <p>아직 작성된 일기가 없어요</p>
+          <p>일기를 작성하러 갈까요?</p>
+        </div>
+
+        <Button variant="complete" className="h-10 w-35 lg:w-45">
+          일기쓰러 가기
+        </Button>
+      </div>
+    );
+  }
+
+  const emotionData = emotions.find((e) => e.variant === diary.emotion);
+
+  return (
+    <div className="m-5 flex items-center gap-4 rounded-md bg-[oklch(0.937_0_0)] p-5 md:m-0 md:w-full md:flex-col md:items-start md:gap-5 md:p-5 lg:gap-5 lg:p-10">
+      {emotionData && (
+        <div className="md:self-center">
+          <emotionData.Icon
+            className={`h-16 w-16 md:h-20 md:w-20 lg:h-24 lg:w-24 ${emotionData.color}`}
+          />
+        </div>
+      )}
+
+      <p className="hidden w-full text-center text-base text-gray-600 md:block md:text-lg">
+        {new Date(diary.date).getMonth() + 1}월 {new Date(diary.date).getDate()}
+        일 일기
+      </p>
+
+      <div className="flex w-full flex-col gap-2 md:gap-5">
+        <div className="flex justify-between">
+          <h1 className="text-lg font-semibold md:mt-4 md:text-xl lg:mt-8 lg:text-2xl">
+            {diary.title}
+          </h1>
+          <p className="text-md hidden text-right text-gray-500 md:hidden">
+            {diary.date}
+          </p>
+        </div>
+
+        <div className="flex items-end justify-between md:flex-col md:items-start">
+          <p className="w-32 truncate text-sm md:w-full md:text-base">
+            {diary.content}
+          </p>
+          <p className="text-right text-sm text-gray-400 md:hidden">
+            {diary.date}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
