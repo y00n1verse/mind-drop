e48b3872b71138a3ae5d2808212701542367d5a2
홍지윤 <jiphrodite@gmail.com>
2025-10-30 15:52:51 +0900

    feat: FormTextarea 구현
    - DiaryForm에서 textarea만을 담당하는 컴포넌트

---
+++ src/app/components/diary/FormTextarea.tsx
@@ -0,0 +1,37 @@
+import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
+import clsx from 'clsx';
+
+interface FormTextareaProps {
+  placeholder: string;
+  register: UseFormRegisterReturn;
+  error?: FieldError;
+  className?: string;
+}
+
+export default function FormTextarea({
+  placeholder,
+  register,
+  error,
+  className,
+}: FormTextareaProps) {
+  return (
+    <div className="flex flex-col">
+      <textarea
+        {...register}
+        placeholder={placeholder}
+        className={clsx(
+          'h-60 w-full resize-none border-b px-3 py-2 placeholder:text-gray-400 focus:outline-none',
+          error
+            ? 'border-[var(--color-warn-bg)] focus:border-[var(--color-warn-bg)]'
+            : 'border-gray-500 focus:border-[var(--color-brand-primary-hover)]',
+          className,
+        )}
+      />
+      {error && (
+        <p className="mt-2 ml-2 text-left text-base text-[var(--color-warn-bg)]">
+          {error.message}
+        </p>
+      )}
+    </div>
+  );
+}
