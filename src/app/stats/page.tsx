72a5b4365585ef0a419e81d2e6ae2523375aa227
홍지윤 <jiphrodite@gmail.com>
2025-11-02 22:56:29 +0900

    feat: EmotionReportPage에 월별 감정 차트와 감정 요약 카드 컴포넌트 렌더링

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,3 +1,15 @@
-export default function StatsPage() {
-  return <div>통계 페이지</div>;
+import EmotionMonthlyChart from './components/EmotionMonthlyChart';
+import EmotionSummaryCard from './components/EmotionSummaryCard';
+
+export default function EmotionReportPage() {
+  return (
+    <div className="mx-auto mb-15 flex w-full max-w-6xl flex-col items-center justify-center gap-10 p-6 lg:flex-row lg:items-start lg:justify-around">
+      <div className="w-full lg:w-[50%]">
+        <EmotionMonthlyChart />
+      </div>
+      <div className="w-full lg:w-[50%]">
+        <EmotionSummaryCard />
+      </div>
+    </div>
+  );
 }
