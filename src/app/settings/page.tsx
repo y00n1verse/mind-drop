75f847c74da44ab2c04cc76fd7de3c71fb9b2f30
홍지윤 <jiphrodite@gmail.com>
2025-11-03 22:19:43 +0900

    feat: 로그아웃 기능 추가 및 AuthGuard를 통해 비로그인 접근시 리다이렉트

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,3 +1,21 @@
+'use client';
+
+import { signOut } from 'next-auth/react';
+import Button from '../components/common/Button';
+import AuthGuard from '../components/AuthGuard';
+
 export default function SettingsPage() {
-  return <div>설정 페이지</div>;
+  return (
+    <AuthGuard>
+      <div className="p-6">
+        <Button
+          variant="warn"
+          onClick={() => signOut({ callbackUrl: '/signin' })}
+          className="cursor-pointer"
+        >
+          로그아웃
+        </Button>
+      </div>
+    </AuthGuard>
+  );
 }
