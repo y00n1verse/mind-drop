cbc131aab6cf8c2340e9cafa0a5e9a8227be9e70
홍지윤 <jiphrodite@gmail.com>
2025-11-04 03:47:50 +0900

    feat: oauth 로그인 시 비밀번호 변경 버튼 숨김 및 로그인 소셜 종류 표시

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import useModal from '../hooks/useModal';
-import { signOut } from 'next-auth/react';
 import AuthGuard from '../components/AuthGuard';
 import Button from '../components/common/Button';
+import { signOut, useSession } from 'next-auth/react';
 import ChangePasswordModal from './components/ChangePasswordModal';
 
 export default function SettingsPage() {
+  const { data: session } = useSession();
   const { isOpen, openModal, closeModal } = useModal();
+
+  const provider = session?.user?.provider;
+  const isSocialLogin = provider && provider !== 'credentials';
 
   const handleDeleteAccount = async () => {
     if (!confirm('정말 탈퇴하시겠습니까? 작성했던 모든 일기가 삭제됩니다.'))
@@ -31,9 +35,17 @@ export default function SettingsPage() {
   return (
     <AuthGuard>
       <div className="flex flex-col gap-4">
-        <Button variant="warn" onClick={openModal} className="cursor-pointer">
-          비밀번호 변경
-        </Button>
+        {!isSocialLogin ? (
+          <Button variant="warn" onClick={openModal} className="cursor-pointer">
+            비밀번호 변경
+          </Button>
+        ) : (
+          <div>
+            {provider === 'google' && 'Google 로그인 계정'}
+            {provider === 'kakao' && 'Kakao 로그인 계정'}
+            {provider === 'naver' && 'Naver 로그인 계정'}
+          </div>
+        )}
         <Button
           variant="warn"
           onClick={() => signOut({ callbackUrl: '/signin' })}
