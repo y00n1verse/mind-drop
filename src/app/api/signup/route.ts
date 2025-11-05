b61bf00a6deb5ba7da80eac4832f0a3e358f6f7a
홍지윤 <jiphrodite@gmail.com>
2025-11-05 12:34:05 +0900

    fix: API에서 Request -> NextRequest로 수정

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   try {
     const { email, password, nickname } = await request.json();
 
