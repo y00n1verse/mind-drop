97be03884839e53532ff733d6327812943d846e8
홍지윤 <jiphrodite@gmail.com>
2025-11-05 13:09:52 +0900

    fix: authOption 파일 분리 및 import 경로 수정

--- src/app/api/auth/change-password/route.ts
+++ src/app/api/auth/change-password/route.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs';
 import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '../[...nextauth]/authOptions';
 
 export async function POST(req: NextRequest) {
   const session = await getServerSession(authOptions);
