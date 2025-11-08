f97487f0e58bf9e2ffcc9e3d2aebd5ab077169fc
홍지윤 <jiphrodite@gmail.com>
2025-11-08 18:57:56 +0900

    feat: fullPage.js 기본 스크롤 및 애니메이션 테스트

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,35 @@
-export default function HomePage() {
-  return <h1>메인 페이지</h1>;
+'use client';
+
+import 'fullpage.js/dist/fullpage.css';
+import ReactFullpage from '@fullpage/react-fullpage';
+
+export default function LandingPage() {
+  return (
+    // @ts-expect-error
+    <ReactFullpage
+      scrollingSpeed={700}
+      easing="easeInOutCubic"
+      navigation
+      fitToSection={true}
+      autoScrolling={true}
+      scrollBar={false}
+      licenseKey="OPEN-SOURCE-GPLV3-LICENSE"
+      render={() => (
+        <ReactFullpage.Wrapper>
+          <div className="section flex h-screen items-center justify-center bg-[var(--background-tertiary)] text-xl">
+            1 페이지
+          </div>
+          <div className="section flex h-screen items-center justify-center bg-[var(--background-tertiary)] text-xl">
+            2 페이지
+          </div>
+          <div className="section flex h-screen items-center justify-center bg-[var(--background-tertiary)] text-xl">
+            3 페이지
+          </div>
+          <div className="section flex h-screen items-center justify-center bg-[var(--background-tertiary)] text-xl">
+            4 페이지
+          </div>
+        </ReactFullpage.Wrapper>
+      )}
+    />
+  );
 }
