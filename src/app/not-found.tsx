06eff97211657cc5b570b3d47a674a2ad49e9270
홍지윤 <jiphrodite@gmail.com>
2025-11-10 00:34:18 +0900

    feat: 404 페이지 국제화

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,20 +2,20 @@
 
 import Link from 'next/link';
 import { HomeIcon } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 import NotFoundIcon from '@/assets/icons/NotFoundIcon.svg';
 
 export default function NotFound() {
+  const { t } = useTranslation();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-center">
       <div className="flex flex-col items-center gap-6">
         <NotFoundIcon />
         <p className="mt-2 mb-4 text-4xl text-gray-600">404 Error</p>
         <div className="mb-2 flex flex-col gap-1 text-base text-gray-800">
-          <p className="text-base">
-            페이지를 찾을 수 없어요.
-            <br />
-            페이지가 존재하지 않거나, 사용할 수 없는 페이지에요. <br />
-            입력하신 주소가 정확한지 다시 한 번 확인해 주세요.
+          <p className="text-base whitespace-pre-line">
+            {t('notFound.description')}
           </p>
         </div>
         <Link
@@ -23,7 +23,7 @@ export default function NotFound() {
           className="flex items-center justify-center gap-2 rounded-lg bg-[var(--color-brand-primary)] px-5 py-3 text-white transition hover:bg-[var(--color-brand-primary-hover)]"
         >
           <HomeIcon className="h-5 w-5" />
-          메인으로 돌아가기
+          {t('notFound.button')}
         </Link>
       </div>
     </main>
