061e36dc8eb3314dae3f73ba2bbe3d9793569587
홍지윤 <jiphrodite@gmail.com>
2025-11-10 16:17:50 +0900

    feat: 로그인 확인 중 로딩 스피너 추가

--- src/app/components/AuthGuard.tsx
+++ src/app/components/AuthGuard.tsx
@@ -1,13 +1,25 @@
 'use client';
 
-import { redirect } from 'next/navigation';
+import Spinner from '../loading';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
-  if (status === 'loading') return <p>로딩 중...</p>;
-  if (!session?.user) redirect('/signin');
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      router.replace('/signin');
+    }
+  }, [status, router]);
+
+  if (status === 'loading') {
+    return <Spinner variant="fade" />;
+  }
+
+  if (!session?.user) return null;
 
   return <>{children}</>;
 }
